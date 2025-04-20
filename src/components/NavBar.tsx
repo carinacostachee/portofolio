b5030c17f32a103b78d96593ffd53421b9102865
Carina Costache <sorincx@Carina-C---MacBook-Air.local>
2025-04-20 13:14:05 +0300

    feat: created contact page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,15 +3,12 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row justify-between items-between border-2 border-rose-800 ">
-      <Link
-        to="/"
-        className="text-2xl font-arcade text-rose-300 mt-6  size={50}"
-      >
+    <div className="flex flex-row justify-between items-center border-2 border-rose-800 ">
+      <Link to="/" className="text-2xl font-arcade text-rose-300  ml-5">
         {"<"}Carina{"/>"}
       </Link>
       <Link to="/tablecontents">
-        <Ellipsis className=" text-rose-300 mt-6" size={50} />
+        <Ellipsis className=" text-rose-300 mr-5" size={60} />
       </Link>
     </div>
   );
--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,11 +1,47 @@
+import { Mail } from "lucide-react";
+import { GitMerge } from "lucide-react";
+import { LuLinkedin } from "react-icons/lu";
+
 const ContactMe = () => {
   return (
     <div className="bg-gradient-to-br from-rose-100 via-white to-rose-200 h-screen">
-      <div className="flex flex-col justyfy-center items-start h-screen gap-5">
+      <div className="flex flex-col justyfy-center items-start h-screen  w-full  ">
         <h1 className="text-[70px] font-arcade tracking-wider text-start text-rose-300 mt-40 ml-40">
           Contact{" "}
         </h1>
+        <hr className="border-rose-800 border-t-[5px] w-1/2 " />
+        <div className="flex flex-row gap-10">
+          <Mail className="text-rose-800 ml-25 mt-10 " size={50} />
+          <a
+            href="mailto:carinacostache@gmail.com"
+            className="font-inconsolata text-rose-300 hover:underline mt-10 text-[30px] font-bold "
+          >
+            carinacostache@gmail.com
+          </a>
+        </div>
+        <div className="flex flex-row gap-10">
+          <GitMerge className="text-rose-800 ml-25 mt-10" size={50} />
+          <a
+            href="https://github.com/carinacostachee"
+            className="font-inconsolata text-rose-300 hover:underline mt-10 text-[30px] font-bold"
+          >
+            check my Github account
+          </a>
+        </div>
+        <div className="flex flex-row gap-10">
+          <LuLinkedin className="text-rose-800 ml-25 mt-10" size={50} />
+          <a
+            href="https://www.linkedin.com/in/carina-costache-1505a3247/"
+            className="font-inconsolata text-rose-300 hover:underline mt-10 text-[30px] font-bold"
+          >
+            and my Linkedin account ;)
+          </a>
+        </div>
       </div>
+      <img
+        src="/photos/eusocial.png"
+        className="absolute top-85 right-50 w-90"
+      ></img>
     </div>
   );
 };
