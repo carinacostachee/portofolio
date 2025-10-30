2fc0e0be2b822c4788db06f933959ad7ea5253f7
Carina Costache <carinacostache@gmail.com>
2025-10-30 13:09:32 +0100

    chore: changed the paths of the font files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          arcade: ["ArcadeClassic", "sans-serif"], 
-        },
-        animation: {
-          typewriter: 'typewriter 2s steps(11) forwards',
-          caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
-        },
-        keyframes: {
-          typewriter: {
-            to: {
-              left: '100%',
-            },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        arcade: ["ArcadeClassic", "sans-serif"],
+        inconsolata: ["Inconsolata", "monospace"],
+        pixel: ["CoralPixel", "sans-serif"],
+        sans: ["Pixelify", "sans-serif"],
+        pixelated: ["FontPixel", "sans-serif"],
+      },
+      colors: {
+        terminal: "rgba(30,30,30,255)",
+      },
+
+      animation: {
+        typewriter: "typewriter 2s steps(11) forwards",
+        caret:
+          "typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s",
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%",
           },
-          blink: {
-            '0%': {
-              opacity: '0',
-            },
-            '0.1%': {
-              opacity: '1',
-            },
-            '50%': {
-              opacity: '1',
-            },
-            '50.1%': {
-              opacity: '0',
-            },
-            '100%': {
-              opacity: '0',
-            },
+        },
+        blink: {
+          "0%": {
+            opacity: "0",
+          },
+          "0.1%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "1",
+          },
+          "50.1%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "0",
           },
         },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
