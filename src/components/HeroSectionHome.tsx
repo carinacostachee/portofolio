c73252b4d7a0e1216c7568d21c2be48b95be5614
Carina Costache <carinacostache@gmail.com>
2025-10-31 11:45:20 +0100

    feat: added terminal input for the site to be a bit more interactive

--- src/components/HeroSectionHome.tsx
+++ src/components/HeroSectionHome.tsx
@@ -4,13 +4,24 @@ import { useState } from "react";
 const HeroSectionHome = () => {
   const [showFolders, setShowFolders] = useState(false);
 
-  const handleFolderClick = () => {
+  const handleFolder = () => {
     setShowFolders(!showFolders);
   };
+
+  const triggerFolderDisplay = () => {
+    const input = document.getElementById("ls_folders") as HTMLInputElement;
+    if (input && input.value.toLocaleLowerCase() === "ls") {
+      handleFolder();
+    }
+  };
+
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-rose-100 via-white to-rose-200 overflow-hidden">
       <div className="flex flex-col sm:flex-row md:flex-col lg:flex-row justify-center items-center h-screen gap-10">
-        <div className="relative w-11/12 h-1/3 md:w-2/3 lg:w-1/2 xl:w-1/3 p-6 rounded-lg bg-terminal flex flex-col items-start gap-8 md:gap-20 overflow-hidden">
+        <div className="font-inconsolata text-rose-300 font-bold">
+          Type ls in the terminal to see secret folders!
+        </div>
+        <div className=" relative w-11/12 h-1/3 md:w-2/3 lg:w-1/2 xl:w-1/3 p-6 rounded-lg bg-terminal flex flex-col items-start gap-8 md:gap-20 overflow-hidden">
           <div className="flex space-x-2 mt-0">
             <div className="w-3 h-3 rounded-full bg-red-500"></div>
             <div className="w-3 h-3 rounded-full bg-yellow-400"></div>
@@ -32,15 +43,20 @@ const HeroSectionHome = () => {
             <div>
               {" "}
               {">"}
-              <span
-                className="hover:underline cursor-pointer"
-                onClick={handleFolderClick}
-              >
-                ls
+              <span>
+                <label htmlFor="ls_folders" className="text-white"></label>
+                <input
+                  type="text"
+                  id="ls_folders"
+                  placeholder="..."
+                  onInput={triggerFolderDisplay}
+                  required
+                />
               </span>
             </div>
           </div>
         </div>
+
         {showFolders && (
           <div className="mt-1  text-rose-300 flex flex-row sm:flex-col gap-4  justify-center items-center">
             <div className="flex flex-col items-center">
