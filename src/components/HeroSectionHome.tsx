2a8ad58fc9525613d0fff2a34770b1a297189e08
Carina Costache <carinacostache@gmail.com>
2025-11-03 13:39:48 +0100

    feat: added png picture to make the macbook vibe

--- src/components/HeroSectionHome.tsx
+++ src/components/HeroSectionHome.tsx
@@ -17,93 +17,127 @@ const HeroSectionHome = () => {
 
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-rose-100 via-white to-rose-200 overflow-hidden ">
-      <div className="flex flex-col lg:flex-row justify-center items-center min-h-screen gap-10 p-4 z-12">
-        <div className="flex flex-col items-center gap-4">
-          <div className="font-inconsolata text-rose-300 font-bold text-2xl sm:4xl">
-            Type 'ls' in the terminal to see secret folders!
-          </div>
-          <div className="relative w-11/12 md:w-full p-6 rounded-lg bg-terminal flex flex-col items-start gap-8 md:gap-20 overflow-hidden z-12">
-            <div className="flex space-x-2 mt-0">
-              <div className="w-3 h-3 rounded-full bg-red-500"></div>
-              <div className="w-3 h-3 rounded-full bg-yellow-400"></div>
-              <div className="w-3 h-3 rounded-full bg-green-500"></div>
+      <div className="grid grid-cols-4">
+        <div className=" flex flex-col justify-start items-start gap-4 col-span-1 ml-10 mt-4">
+          <div className="flex flex-col items-center gap-1">
+            <img
+              src="/photos/Lolita.png"
+              alt="Lolita postacard"
+              className="w-14 sm:w-40 h-auto"
+            />
+            <div className="font-inconsolata text-black font-bold text-[5px] sm:text-sm ">
+              Lolita_postcard.png
             </div>
-            <div className="font-inconsolata text-white text-xl sm:text-2xl lg:text-2xl xl:text-xl font-bold">
-              <div>{">"} Hey, I am</div>
-              <div className="w-max">
-                <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-2xl sm:text-5xl text-rose-300 font-arcade">
-                  <span className="font-inconsolata text-white text-xl sm:text-2xl lg:text-3xl xl:text-2xl">
-                    {">"}
-                  </span>
-                  Carina Costache,
-                </h1>
-              </div>
-              <div>{">"} and I am excited to welcome you</div>
-              <div>{">"} to my portofolio website!</div>
-
-              <div>
-                {" "}
-                {">"}user@123~
-                <span>
-                  <label
-                    htmlFor="ls_folders"
-                    className="text-white font-inconsolata"
-                  ></label>
-                  <input
-                    className="focus:outline-none"
-                    type="text"
-                    id="ls_folders"
-                    placeholder="..."
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </span>
-              </div>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <img
+              src="/photos/Ping.png"
+              alt="Human drawing postacard"
+              className="w-14 sm:w-40 h-auto"
+            />
+            <div className="font-inconsolata text-black font-bold text-[5px] sm:text-sm ">
+              Museum_postcard.png
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <img
+              src="/photos/Lana.png"
+              alt="Lana postacard"
+              className="w-14 sm:w-40 h-auto"
+            />
+            <div className="font-inconsolata text-black font-bold text-[5px] sm:text-sm ">
+              Fav_Lana_album.png
             </div>
           </div>
         </div>
+        <div className="col-span-3 flex flex-col lg:flex-row justify-center items-center min-h-screen gap-10 p-4 z-12">
+          <div className="flex flex-col justify-center items-start gap-4">
+            <div className="font-inconsolata text-rose-300 font-bold text-2xl sm:4xl">
+              Type 'ls' in the terminal to see secret folders!
+            </div>
+            <div className="relative w-11/12 md:w-full p-6 rounded-lg bg-terminal flex flex-col items-start gap-8 md:gap-20 overflow-hidden z-12">
+              <div className="flex space-x-2 mt-0">
+                <div className="w-3 h-3 rounded-full bg-red-500"></div>
+                <div className="w-3 h-3 rounded-full bg-yellow-400"></div>
+                <div className="w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <div className="font-inconsolata text-white text-xl sm:text-2xl lg:text-2xl xl:text-xl font-bold">
+                <div>{">"} Hey, I am</div>
+                <div className="w-max">
+                  <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-2xl sm:text-5xl text-rose-300 font-arcade">
+                    <span className="font-inconsolata text-white text-xl sm:text-2xl lg:text-3xl xl:text-2xl">
+                      {">"}
+                    </span>
+                    Carina Costache,
+                  </h1>
+                </div>
+                <div>{">"} and I am excited to welcome you</div>
+                <div>{">"} to my portofolio website!</div>
 
-        {showFolders && (
-          <div className="mt-1  text-rose-300 flex flex-row md:flex-row lg:flex-col sm:flex-col gap-4  justify-center items-center">
-            <div className="flex flex-col items-center">
-              <img
-                src="/photos/pinkfolder.png"
-                className="w-20 sm:w-28 h-auto "
-              ></img>
-              <Link
-                to="/about"
-                className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
-              >
-                Me
-              </Link>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="/photos/pinkfolder.png"
-                className="w-20 sm:w-28 h-auto"
-              ></img>
-              <Link
-                to="/work"
-                className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
-              >
-                Work
-              </Link>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="/photos/pinkfolder.png"
-                className="w-20 sm:w-28 h-auto"
-              ></img>
-              <Link
-                to="/contact"
-                className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
-              >
-                Reach me
-              </Link>
+                <div>
+                  {" "}
+                  {">"}user@123~
+                  <span>
+                    <label
+                      htmlFor="ls_folders"
+                      className="text-white font-inconsolata"
+                    ></label>
+                    <input
+                      className="focus:outline-none"
+                      type="text"
+                      id="ls_folders"
+                      placeholder="..."
+                      value={inputValue}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-        )}
+
+          {showFolders && (
+            <div className="mt-1  text-rose-300 flex flex-row md:flex-row lg:flex-col sm:flex-col gap-4  justify-center items-center">
+              <div className="flex flex-col items-center">
+                <img
+                  src="/photos/pinkfolder.png"
+                  className="w-20 sm:w-28 h-auto "
+                ></img>
+                <Link
+                  to="/about"
+                  className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
+                >
+                  Me
+                </Link>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  src="/photos/pinkfolder.png"
+                  className="w-20 sm:w-28 h-auto"
+                ></img>
+                <Link
+                  to="/work"
+                  className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
+                >
+                  Work
+                </Link>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  src="/photos/pinkfolder.png"
+                  className="w-20 sm:w-28 h-auto"
+                ></img>
+                <Link
+                  to="/contact"
+                  className="text-xl sm:text-3xl font-arcade text-rose-300 hover:underline text-center"
+                >
+                  Reach me
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
